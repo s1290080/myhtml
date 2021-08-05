0eed3bd7fdd921225b9e6ab701c86f24081137d5
NOHARA Kaiki <s1290080@u-aizu.ac.jp>
2021-08-05 12:14:08 +0900

    add.js

---
+++ add.js
@@ -0,0 +1,23 @@
+function flexTextarea(el) {
+  const dummy = el.querySelector('.FlexTextarea__dummy')
+  el.querySelector('.FlexTextarea__textarea').addEventListener('input', e => {
+    dummy.textContent = e.target.value + '\u200b'
+  })
+}
+
+document.querySelectorAll('.FlexTextarea').forEach(flexTextarea)
+
+var matome=[概要]
+var code=[コード]
+var comment=[評価・コメント]
+var matomeme=document.getElementById("1")
+var codede=document.getElementById("2")
+var commentt=document.getElementById("3")
+var jikko=document.getElementById("4")
+jikko.addEventListenner("click",detcli)
+function detcli(){
+    matome.push(matomeme.value)
+    code.push(codede.value)
+    comment.push(commentt.value)
+location.href=index.html
+}
