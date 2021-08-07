39b239edb48ffda6e95ab98207eec64fef55dd09
NOHARA Kaiki <s1290080@u-aizu.ac.jp>
2021-08-07 13:49:14 +0900

    id

--- add.js
+++ add.js
@@ -2,24 +2,24 @@ function flexTextarea(el) {
     const dummy = el.querySelector('.FlexTextarea__dummy')
     el.querySelector('.FlexTextarea__textarea').addEventListener('input', e => {
       dummy.textContent = e.target.value + '\u200b'
-  })
+    })
 }
 
 document.querySelectorAll('.FlexTextarea').forEach(flexTextarea)
 
-var matome=[概要]
-var code=[コード]
-var comment=[評価・コメント]
-var matomeme=document.getElementById("1")
-var codede=document.getElementById("2")
-var commentt=document.getElementById("3")
-var jikko=document.getElementById("4")
+var matome=["概要"]
+var code=["コード"]
+var comment=["評価・コメント"]
+var matomeme=document.getElementById("addtextarea1")
+var codede=document.getElementById("addtextarea2")
+var commentt=document.getElementById("addtextarea3")
+var jikko=document.getElementById("addbutton1")
 jikko.addEventListenner("click",detcli)
 function detcli(){
     detcli1();
     detcli2();
     detcli3();  
-    location.href=index.html
+    location.href="index.html";
 }
 function detcli1(){
     matome.push(matomeme.value);
